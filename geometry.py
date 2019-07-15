bc4affc81a144700f157df3b08073684060b5c82
Aleksandr Kovalchukov <aleksandr.kovalchukov@mail.ru>
2019-07-15 23:50:25 +0300

    work

--- geometry.py
+++ geometry.py
@@ -13,6 +13,7 @@ class VoronovCell:
         center = Point2D(0, 0)
         line = Line2D(center, point)
         midpoint = Point2D(point.x / 2, point.y / 2)
+        self.minBorderDist = min(self.minBorderDist, distToZero(midpoint.x, midpoint.y))
         self.perpendicular = line.perpendicular_line(midpoint)
 
         sectionIntersections = []
@@ -40,7 +41,7 @@ class VoronovCell:
     def updateCell(self, point):
         self.__getIntersections(point)
         if not self.__sections:
-            print("No intersections")
+            #print("No intersections")
             return
 
         for i in self.__sections:
@@ -119,6 +120,7 @@ class VoronovCell:
         # print(shell1)
         # print(shell2)
         #выбираем нужную оболочку
+        flag = False
         for i in shell1:
             #print("t", i)
             if i not in self.perpendicular:
@@ -126,42 +128,108 @@ class VoronovCell:
                 if testSection.intersection(self.perpendicular):
                     self.shell = shell1.copy()
                     self.sizeOfShell = len(shell1)
-                    return
-        self.shell = shell2.copy()
-        self.sizeOfShell = len(shell2)
+                    flag = True
+        if not flag:
+            self.shell = shell2.copy()
+            self.sizeOfShell = len(shell2)
+        ans = 0
+        for i in self.shell:
+            ans = max(ans, distToZero(i.x, i.y))
+        self.maxBorderDist = ans
 
         #print(self.shell)
 
+def distToZero(a, b):
+    return (a*a + b*b)**(1/2)
+
+def insertText(screen, x, n, AV):
+    textsurface = myfont.render(' x = ' + str(x), False, (0, 0, 0))
+    screen.blit(textsurface, (width, 50))
+
+    textsurface = myfont.render(' h = ' + str(H) + '/' + str(n), False, (0, 0, 0))
+    screen.blit(textsurface, (width, 150))
+
+
+    textsurface = myfont.render(' A(V) = ' + str(format(AV, '.10g')), False, (0, 0, 0))
+    screen.blit(textsurface, (width, 250))
 
 import sys, pygame
 pygame.init()
 
-size = width, height = 1000, 1000
+width, height = 1000, 1000
+size = width + 300, height
 black = 0, 0, 0
+gray = 127, 127, 127
 white = 255, 255, 255
 
 screen = pygame.display.set_mode(size)
 
 
-screen.fill(white)
-pygame.draw.line(screen, black, (500, 0), (500, 1000), 1)
-pygame.draw.line(screen, black, (0, 500), (1000, 500), 1)
 
-x = 0.618
-h = 0.2
+
+#x = 0.61803398875
+#x = 3.1415926
+#x = 1.4331274267223
+x = 2**(1/2)
+#x = 1/3
+H, h = 1, 1
+STEP = 500
+
 cell = VoronovCell()
-for i in range(-5, 5 + 1):
-    for j in range(-5, 5 + 1):
-        if(i != 0 and  j != 0):
-            point = Point2D(i *x*j, j*h)
-            cell.updateCell(point)
+A = []
+for i in range(-1000, 1000 + 1):
+    for j in range(-1000, 1000 + 1):
+        a, b = i + x * j, j
+        r = distToZero(a, b)
+        if abs(a) <= 2:
+            A.append([r, a, b, 0])
+A.sort()
+
+for n in range(1, 50, 1):
+    step = STEP * (n**(1/2)) / 3
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: sys.exit()
+
+    screen.fill(white)
+    pygame.draw.line(screen, black, (500, 0), (500, 1000), 1)
+    pygame.draw.line(screen, black, (0, 500), (1000, 500), 1)
+    pygame.font.init()
+    myfont = pygame.font.SysFont('Arial', 30)
+
+    h = H / n
+
+    for i in A:
+        if abs(i[1])*step > width / 2:
+            A.remove(i)
+
+    for i in range(len(A)):
+        #print(A[i])
+        A[i][3] = A[i][2] * h
+        A[i][0] = distToZero(A[i][1], A[i][3])
+    A.sort()
+
+    for i in A:
+        if(abs(i[1] * step) <= width / 2 and abs(i[3] * step) < height / 2):
+            pygame.draw.circle(screen, black, (int(i[1] * step + width / 2), height - int(i[3] * step + height / 2)), 1)
+        if i[0] <= 2 * cell.maxBorderDist:
+            if(i[1] != 0 or i[3] != 0):
+                point = Point2D(i[1], i[3])
+                cell.updateCell(point)
+        #else:
+            #break
 
 
-shape = cell.shell.copy()
-for i in range(len(shape)):
-    shape[i] = (shape[i].x * 200 + 500, shape[i].y * 200 + 500)
 
-pygame.draw.polygon(screen, black, shape, 1)
-pygame.display.flip()
-pygame.time.delay(50000)
+
+    AV = cell.maxBorderDist / cell.minBorderDist
+    shape = cell.shell.copy()
+    for i in range(len(shape)):
+        shape[i] = (shape[i].x * step + 500, height - (shape[i].y * step + 500))
+    pygame.draw.polygon(screen, black, shape, 1)
+
+    pygame.draw.rect(screen, gray, ((width, 0),(width + 200, height)), 0)
+    insertText(screen, x, n, AV)
+    pygame.display.flip()
+    #pygame.image.save(screen, "./sqrt(2)/screenshot" + str(n) + ".jpeg")
+
 
